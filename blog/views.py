18697d13e946e8b922709271545804330ab9a0b4
Mr.RahmanPour <Behshad.RahmanPour@gmail.com>
2022-08-12 18:17:00 +0430

    update index page

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,10 @@ from .models import Essays
 
 # Create your views here.
 
+
 def index(request):
 
     essay = Essays.objects.all()
 
-    context = {'essay':essay}
-    return render(request , 'src/index.html' , context)
+    context = {'essay': essay}
+    return render(request, 'src/index.html', context)
