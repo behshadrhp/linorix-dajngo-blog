9ba34145b547903a683b5c06fe1b2a28a0d65835
Mr.RahmanPour <Behshad.RahmanPour@gmail.com>
2022-08-12 18:34:27 +0430

    create view-essay page

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import index , view_essay
 
 
 urlpatterns = [
 
     # index page
     path('' , index , name='index'),
+
+    # view essay
+    path('view-essay/<slug:pk>' , view_essay , name='view-essay'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,18 @@ from .models import Essays
 
 
 def index(request):
+    # This function is for developing and making changes to the index file
 
     essay = Essays.objects.all()
 
     context = {'essay': essay}
     return render(request, 'src/index.html', context)
+
+
+def view_essay(request , pk):
+    # This function is for developing and making changes to the view essay file
+
+    essay = Essays.objects.get(slug=pk)
+
+    context = {'essay':essay}
+    return render(request, 'src/view_essay.html' , context)
