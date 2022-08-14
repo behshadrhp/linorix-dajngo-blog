c1893e36c47295409c204dceca530303f63e355d
Mr.RahmanPour <Behshad.RahmanPour@gmail.com>
2022-08-14 15:28:53 +0430

    create postgresql and switch to postgresql database

--- core/settings.py
+++ core/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "tedora",
+        "USER": "behshad",
+        "PASSWORD": "#Mr-RahmanPour-2004-9814*45*83",
+        "HOST": "localhost",
+        "PORT":'',
     }
 }
 
