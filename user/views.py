6b26ed3ac70b19c9b778d08d3a5857e43b28b01c
Mr.RahmanPour <Behshad.RahmanPour@gmail.com>
2022-08-24 23:19:26 +0430

    create user page

--- user/views.py
+++ user/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 
 def user(request):
-    return render(request, 'src/user.html')
+    # This function is for profile user
+
+    users = Profile.objects.all()
+
+    context = {'users':users}
+    return render(request, 'src/user.html', context)
