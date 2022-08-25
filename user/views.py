39649994b274f8e9df863f9b00cac76c0d56c05b
Mr.RahmanPour <Behshad.RahmanPour@gmail.com>
2022-08-25 11:43:40 +0430

    complate skills

--- user/views.py
+++ user/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile, Skill
+from blog.models import Essays
 
 # Create your views here.
+
 
 def user(request):
     # This function is for profile user
 
+    # user
     users = Profile.objects.all()
 
-    context = {'users':users}
+    context = {'users': users}
     return render(request, 'src/user.html', context)
+
 
 def user_profile(request, pk):
     # This function is for view user profile
 
+    # username filter
     users = Profile.objects.get(username=pk)
 
-    context = {'users':users}
+    # Skills
+    top_skill = users.skill_set.exclude(description='')
+    other_skill = users.skill_set.filter(description='')
+
+    context = {'users': users, 'top_skill': top_skill,
+               'other_skill': other_skill}
     return render(request, 'src/profile.html', context)
