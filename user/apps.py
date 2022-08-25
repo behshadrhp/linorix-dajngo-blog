d5a62b456fc32baf4577b52c57317dd64d70efcf
Mr.RahmanPour <Behshad.RahmanPour@gmail.com>
2022-08-25 17:24:43 +0430

    create signals

--- user/apps.py
+++ user/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "user"
+    
+    # Enable Signals
+    def ready(self):
+        import user.signals
---
+++ user/signals.py
@@ -0,0 +1,32 @@
+from django.db.models.signals import post_save, post_delete
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+# create signal models
+
+# Create user Profile 
+def create_profile(sender, instance, created, **kwargs):
+
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            full_name=user.first_name,
+            username=user.username,
+            email=user.email,
+            
+        )
+
+# delete user Profile
+def delete_profile(sender, instance, **kwargs):
+
+    user = instance.user
+    user.delete()
+
+
+# connection for Create user Profile
+post_save.connect(create_profile, sender=User)
+
+# connection for Delete user Profile
+post_delete.connect(delete_profile, sender=Profile)
