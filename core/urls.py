52be517bf3b92dd3b54970260990e57817a71ddd
MrRahmanPour <Behshad.RahmanPour@gmail.com>
2022-09-17 09:57:43 +0430

    change url admin panel

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("linorix-panel-me/", admin.site.urls),
 
     # include blog app
     path('', include('blog.urls')),
