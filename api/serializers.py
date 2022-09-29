9523c80a0d67d90b2afd48f3fe35c360c2c9353b
MrRahmanPour <Behshad.RahmanPour@gmail.com>
2022-09-29 11:08:10 +0330

    add [profile,tag,comment] to serializers file

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
-from blog.models import Essay
+from blog.models import Essay, Tag, Comment
+from user.models import Profile
+
+class ProfileSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class TagSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class CommentSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
 
 class EssaySerializers(serializers.ModelSerializer):
+    owner = ProfileSerializers(many=False)
+    tag = TagSerializers(many=True)
+    comment = serializers.SerializerMethodField()
     class Meta:
         model = Essay
         fields = '__all__'
+
+    def get_comment(self, obj):
+
+        comment = obj.comment_set.all()
+        serializers = CommentSerializers(comment, many=True)
+
+        return serializers.data
